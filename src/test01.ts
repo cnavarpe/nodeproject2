0e94de3098330e9569e03a4265bb22743bc0203d
Carolina <cnavarpe@gmail.com>
2024-11-09 21:23:08 +0100

    adding files

---
+++ src/test01.ts
@@ -0,0 +1,26 @@
+
+
+function testing01() {
+    console.log('Hello World!');
+}
+
+function undefinedVersusNull(){
+    /*var testVar;
+    console.log(testVar);
+    console.log(typeof testVar);
+    console.log(testVar == undefined);
+    console.log(testVar === undefined);*/
+
+    var testVar = undefined;
+    console.log(testVar);
+    console.log(typeof testVar);
+    console.log(testVar == undefined);
+    console.log(testVar === undefined);
+    console.log(testVar == null);
+    console.log(testVar === null);
+
+
+}
+
+
+undefinedVersusNull();
